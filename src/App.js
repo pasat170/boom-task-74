917a779a3106eec70679a9dd9ed7cb55cbaf1089
Stefan Poibrenski <stefan_poibrenski@abv.bg>
2023-01-11 22:16:22 +0200

    feat: add validation

--- src/App.js
+++ src/App.js
@@ -1,21 +1,37 @@
 import "./App.css";
-
+import { useState, useMemo } from "react";
 function App() {
-  return (
-    <div className="App">
-     <div className="control has-icons-right">
-        <input
-          className="input is-large"
-          type="text"
-          placeholder="Enter number..."
+    let [text, setText] = useState("");
+    let tru = false;
+    const reg = new RegExp("^[0-9]+$")
+    const check =
+        useMemo(() => {
+            if (reg.test(text) == false) {
+                tru = false;
+            } else {
+                tru = true;
+            }
+        }, [text])
 
-        />
-        <span className="icon is-small is-right">
-          <i className="fas fa-times" />
-        </span> 
-      </div>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div className="control has-icons-right">
+                <input
+                    className="input is-large"
+                    type="text"
+                    placeholder="Enter number..."
+                    value={text}
+                    onChange={
+                        (e) => {
+                            setText(e.target.value)
+                        }}
+                />
+                <span className="icon is-small is-right">
+                    <i className={tru ? "fas fa-check" : "fas fa-times"} />
+                </span>
+            </div>
+        </div>
+    );
 }
 
 export default App;
